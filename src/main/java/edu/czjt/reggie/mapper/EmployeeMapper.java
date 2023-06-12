b9a9c92494f0e7471c585590007477aecfa7c6bc
huxue <739206147@qq.com>
2023-06-12 23:42:52 +0800

    分类管理和添加新功能 胡雪

---
+++ src/main/java/edu/czjt/reggie/mapper/EmployeeMapper.java
@@ -0,0 +1,34 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import edu.czjt.reggie.entity.Employee;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+
+
+
+/**
+ *
+ *
+ *
+ * Created by jinkun.tian on 2023/3/16
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+@Mapper
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+
+
+
+
+}
---
+++ src/main/java/edu/czjt/reggie/mapper/SetmealDishMapper.java
@@ -0,0 +1,31 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+
+import edu.czjt.reggie.entity.SetmealDish;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+@Mapper
+public interface SetmealDishMapper extends BaseMapper<SetmealDish> {
+
+
+
+
+
+
+
+}
